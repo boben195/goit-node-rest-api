fce692de37c5cd99ef7d4014fd14d5f8c9bae7e8
boben195 <boben195@ukr.net>
2024-06-06 11:47:57 +0300

    delete comments and conole.log

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,36 +3,12 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-// export const auth = async (req, res, next) => {
-//     const { authorization = "" } = req.headers;
-//     console.log("Authorization Header:", authorization);
-    
-//     const [bearer, token] = authorization.split(" ");
-//  console.log("Bearer:", bearer);
-//  console.log("Token:", token); 
-//     if (bearer !== "Bearer") {
-//         return res.status(401).send({message: "Not authorized"})
-//     }
-//     try {
-//         const { id } = jwt.verify(token, JWT_SECRET);
-//         const user = await User.findById(id)
-//         if (!user || !user.token || user.token !== token) {
-//         return res.status(401).send({message: "Not authorized"})
-//         }
-//         req.user = user;
-//         next()
-
-
-//     } catch(error) {
-//         next(error)
-//     }
-// }
 
 
 export const auth = async (req, res, next) => {
     const { authorization = "" } = req.headers;
 
-    console.log("Authorization Header:", authorization);
+    
 
     if (!authorization) {
         return res.status(401).send({ message: "Authorization header missing" });
@@ -40,8 +16,7 @@ export const auth = async (req, res, next) => {
 
     const [bearer, token] = authorization.split(" ");
     
-    console.log("Bearer:", bearer);
-    console.log("Token:", token);
+    
 
     if (bearer !== "Bearer" || !token) {
         return res.status(401).send({ message: "Invalid authorization format" });
